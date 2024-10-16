6f94352ba68cb97595dd1becb61911b3169094fd
Gargenziano03 <gargenziano03@gmail.com>
2024-10-16 17:26:10 +0200

    add snack 8 finish

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,5 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const classMarc = students.find(student => student.name === 'Marco Lanci')['class'];
+console.log(classMarc);
