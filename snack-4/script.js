080c4ab0aed3be71dfa9a6572f4c597a13c21199
Gargenziano03 <gargenziano03@gmail.com>
2024-10-16 16:41:01 +0200

    add snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+posts.forEach(post => {
+  console.log(post['author']);
+});
