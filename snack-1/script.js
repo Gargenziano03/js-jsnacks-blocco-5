479b8434ad96dc41bef2a91b2d8ea0864dbbfcdb
Gargenziano03 <gargenziano03@gmail.com>
2024-10-16 15:46:36 +0200

    add snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//ciclo nel array per stampare i nomi
+names.forEach(name => {
+    console.log(name);
+});
